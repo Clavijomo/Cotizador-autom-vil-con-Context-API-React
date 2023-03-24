5b800c6b0cb457214e6aa12dfb76a55dcfc10bec
Jonathan Clavijo <jonathanclavijo0907@gmail.com>
2023-03-24 14:33:58 -0500

    :rocket: Finalizacion del proyecto de Cotizador de autos con Context en React

--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -1,3 +1,4 @@
+import {useCallback, useMemo, useRef} from 'react'
 import { MARCAS, PLANES } from "../constants"
 import useCotizador from "../hooks/useCotizador"
 
@@ -6,8 +7,18 @@ const Resultado = () => {
   const { resultado, datos } = useCotizador();
   const {marca, plan, year} = datos;
   
-  const [nombreMarca] = MARCAS.filter(m => m.id === Number(marca));
+  const yearRef = useRef(year);
+  
+  const [nombreMarca] = useMemo( () =>
+    MARCAS.filter(m => m.id === Number(marca)), 
+    [resultado]
+  )
 
+  const [nombrePlan] = useMemo( () =>
+    PLANES.filter(p => p.id === Number(plan)),
+    [resultado]
+  )
+  
   if(resultado === 0) return null;
 
   return (
@@ -20,6 +31,22 @@ const Resultado = () => {
         <span className="font-semibold">Marca: </span>
         {nombreMarca.nombre}
       </p>
+
+      <p className="my-2">
+        <span className="font-semibold">Plan: </span>
+        {nombrePlan.nombre}
+      </p>
+
+      <p className="my-2">
+        <span className="font-semibold">Año del auto: </span>
+        {yearRef.current}
+      </p>
+
+      <p className="my-5 text-2xl">
+        <span className="font-semibold">Total de la cotización: </span>
+        {resultado}
+      </p>
+      
     </div>
   )
 }
